4ed4aae444d26b57d7663e7b06aaea15e1636149
stsoul47 <46344256+stsoul47@users.noreply.github.com>
2022-05-30 21:01:12 +0900

    react, react DOM
    react, react DOM update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@
 // 이 컴포넌트가 만든 HTML을 가지고 가고, 페이지에 반영한다.(DOM 안에)
 // why? 리액트에게 내가 만든 컴포넌트를 DOM으로 넣으라고 지시를 해야 함
 
-//잘못된 코드
+import React from 'react'; 
+import ReactDOM from 'react-dom';
+// 코어 리액트 라이브러리 => 리액트 컴포넌트와 어떻게 작동하는지 알고 있다. 
+// 어떻게 렌더링 되며, 어떻게 그것들을 모으는지 등을 아는 것
+// 하지만 이 컴포넌트를 가져와 DOM에 삽입하는 라이브러리는 별도로 React DOM이라고 부른다.
+
+
 const App = function() {
   return <div>HI!</div>;
 }
+
+ReactDOM.render(App);
