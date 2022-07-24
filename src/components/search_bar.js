e3e5314d721141b5afc13b30eb0bcd751c2887da
stsoul47 <46344256+stsoul47@users.noreply.github.com>
2022-07-25 07:37:49 +0900

    class components 적용
    class components 적용

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,10 +4,19 @@
  * 상단의 입력창, 타이핑 가능
  * 검색어 단어를 입력하면, 오른쪽에 비디오 리스트 업데이트가 가능
  */
-import React from 'react'; //jsx를 가지면 모든 컴포넌트에 import를 해줘야 한다.
+import React, { Component } from 'react'; //jsx를 가지면 모든 컴포넌트에 React를 import를 해줘야 한다.
+// 여기서 괄호의 의미는 React라이브러리에서 Component라 부르는 변수를 프로퍼티 형태로 가져오라는 것이다.
 
-const SearchBar = () => {
-  return <input type="text" />; //React.createElement("input", null);
-};
+class SearchBar extends Component {
+
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
+  render() {
+    return <input type="text" onChange={ this.onInputChange } />;
+  }
+}
+
 
 export default SearchBar;
